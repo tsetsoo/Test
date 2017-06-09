80b86103e3a5a02d0d17ce5dd8bcd7a38a6994cc
Tsvetelin Pantev <diablo96bg@gmail.com>
2017-06-09 15:19:54 +0000

    added backup url variable

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var campgroundRoutes = require("./routes/campgrounds"),
       commentsRoutes = require("./routes/comments"),
          indexRoutes = require("./routes/index");
 
-
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost/YelpCamp";
+mongoose.connect(url);
 app.use(express.static(__dirname + "/public/"));
 app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
