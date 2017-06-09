9feb99b5de1ccd5a648078a32f07167292c4f8b6
Tsvetelin Pantev <diablo96bg@gmail.com>
2017-06-09 15:26:11 +0000

    hidden gmaps key final

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
+    res.locals.mapkey = process.env.GMAPSKEY;
     next();
 });
 app.use(methodOverride("_method"));
