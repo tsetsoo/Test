3d67eb97e6a91850a35994bfd7793d0e4c173bdb
Tsvetelin Pantev <diablo96bg@gmail.com>
2017-12-18 22:34:38 +0000

    Final Commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var flash = require("connect-flash");
 var campgroundRoutes = require("./routes/campgrounds"),
       commentsRoutes = require("./routes/comments"),
          indexRoutes = require("./routes/index");
-
+console.log(process.env.DATABASEURL);
 var url = process.env.DATABASEURL || "mongodb://localhost/YelpCamp";
 mongoose.connect(url);
 app.use(express.static(__dirname + "/public/"));
